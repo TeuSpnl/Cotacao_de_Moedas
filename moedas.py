30b13b061712e60442a4343f1de1d6bcb7413826
Spínola <teu_almeida20010@hotmail.com>
2022-06-03 20:05:38 -0300

    Atualizando comentários e dando espaços

--- moedas.py
+++ moedas.py
@@ -43,6 +43,7 @@ def atualizar_arq():
 
     link = f"https://economia.awesomeapi.com.br/USD-BRL/10?start_date={anoInicial}{mesInicial}{diaInicial}&end_date={anoFinal}{mesFinal}{diaFinal}"
 
+    print(link)
     reqCota = requests.get(link)
     cotacao = reqCota.json()
     # moeda = cotacao[0]['code']
@@ -76,10 +77,13 @@ mainTitle = tk.Label(text="Cotação de moedas", font=("Roboto, 50")).grid(
 subTitle1 = tk.Label(text="Cotação de uma única moeda", anchor="w", padx=15, font=(
     "Roboto, 14")).grid(row=1, column=0, sticky="NSEW")
 
-moeda1 = tk.Label(text="Escolha a moeda: ", font=("Roboto, 11")).grid(
+
+moeda1 = tk.Label(text="Escolha as moedas: ", font=("Roboto, 11")).grid(
     row=2, column=0, sticky="NSEW", pady=(10, 0))
+
 selcMoeda1 = ttk.Combobox(values=lista)
 selcMoeda1.grid(row=3, column=0, pady=(0, 10))
+
 
 data1 = tk.Label(text="Escolha a data: ", font=("Roboto, 11")).grid(
     row=2, column=2, sticky="NSEW", pady=(10, 0))
@@ -87,8 +91,10 @@ data1 = tk.Label(text="Escolha a data: ", font=("Roboto, 11")).grid(
 selcData1 = DateEntry(year=2022, locale='pt_br', width=15)
 selcData1.grid(row=3, column=2, pady=(0, 10))
 
+
 botaoBusca = tk.Button(text="Buscar", command=pegar_cotacao, width=10, font="Roboto, 11").grid(
     column=0, row=4, columnspan=6, sticky="NS")
+
 
 result1 = tk.Label(text="", font="Roboto, 11")
 result1.grid(row=5, pady=(10, 20), column=0, columnspan=6, sticky="NSEW")
@@ -98,32 +104,44 @@ result1.grid(row=5, pady=(10, 20), column=0, columnspan=6, sticky="NSEW")
 subTitle2 = tk.Label(text="Cotação de moedas + data", anchor="w", padx=15,
                      font=("Roboto, 14")).grid(row=6, column=0, sticky="NSEW")
 
+
 respcheck = tk.IntVar()
+
 todasounnao = tk.Checkbutton(text="Quero salvar os dados de todas as moedas.", variable=respcheck, command= create_moeda).grid(
     row=7, column=0, columnspan=7)
 
+
 moeda2 = tk.Label(text="Escolha a moeda: ", font=("Roboto, 11"))
 moeda2.grid(row=8, column=0, sticky="NSEW", pady=10)
+
 selcMoeda2 = ttk.Combobox(values=lista, width=15)
 selcMoeda2.grid(row=8, column=1, pady=10, sticky="NSEW")
 
 
 dataInicial = tk.Label(text="Escolha a data: ", font=("Roboto, 11")).grid(
     row=9, column=0, sticky="NSEW", pady=(0, 5))
+
 selcDataInicial = DateEntry(year=2022, locale='pt_br', width=15)
 selcDataInicial.grid(row=9, column=1, sticky="EW")
+
 selcDataFinal = DateEntry(year=2022, locale='pt_br', width=15)
 selcDataFinal.grid(row=10, column=1, sticky="NSEW")
+
+
 
 botaoSalvar = tk.Button(text="Salvar", font="Roboto, 13", width=15, height=2, command=atualizar_arq).grid(
     row=7, column=2, rowspan=3, padx=(50, 0), pady=(5, 0))
 
+
 msgSelcErr = tk.Label(text="",
                       font="Roboto, 12", anchor="w", justify="left")
+
 msgSelcErr.grid(row=11, column=2, sticky="NSEW", pady=(10, 40))
+
 
 janela.mainloop()
 
 
-# Fazer com que o sistema crie um arquivo novo com os dados
+# Pegar selec arquiv (colocar nome de atualizar arquivo) no github
+# Fazer com que o sistema crie um arquivo novo com os dados ou atualize um arquivo
 # Fazer com que o usuário possa escolher se quer todas as moedas ou só algumas
