b3bc849da4be6662d1e3d31bfab5b52da02cc6ba
Spínola <teu_almeida20010@hotmail.com>
2022-06-04 13:30:44 -0300

    readicionando escolher arquivo e adicionando botão +

--- moedas.py
+++ moedas.py
@@ -1,9 +1,10 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ANCHOR, ttk
 from tkinter.filedialog import askopenfilename
 from tkcalendar import DateEntry
 import pandas as pd
 import requests
+import numpy as np
 
 req = requests.get('https://economia.awesomeapi.com.br/json/all')
 dicio = req.json()
@@ -27,8 +28,30 @@ def pegar_cotacao():
     result1["text"] = f"Na data {data}, a moeda {moeda} custava R${valor}"
 
 
+#Seleciona arquivo
+def selecionar():
+    pass
+    caminhoGot = askopenfilename(title= "Selecione o arquivo xls")
+    caminho.set(caminhoGot)
+    if (caminhoGot):
+        msgSelcErr["text"] = f"{caminhoGot}"
+
+# Habilita/desabilita a caixa de seleção de moedas
+def all_or_not():
+    if (respcheck.get() == 0):
+        moeda2['state'] = 'normal'
+        selcMoeda2['state'] = 'normal'
+    else:
+        moeda2['state'] = 'disabled'
+        selcMoeda2['state'] = 'disabled'
+
+def adicionar_moeda():
+    pass
+
 # Atualizar/Criar arquivo para guardar as informações
 def atualizar_arq():
+    todas = respcheck.get()
+    
     dataInicial = selcDataInicial.get()
     anoInicial = dataInicial[-4:]
     mesInicial = dataInicial[3:5]
@@ -38,6 +61,23 @@ def atualizar_arq():
     anoFinal = dataFinal[-4:]
     mesFinal = dataFinal[3:5]
     diaFinal = dataFinal[:2]
+    
+    if (todas == 1):
+        link = f"https://economia.awesomeapi.com.br/json/daily/all?start_date={anoInicial}{mesInicial}{diaInicial}&end_date={anoFinal}{mesFinal}{diaFinal}"
+    else:
+        link = f""    
+
+    if(caminho != "" and todas == 1):
+        pass
+    
+    elif(caminho != "" and todas == 0):
+        pass
+    
+    elif(caminho == "" and todas == 1):
+        link = f"https://economia.awesomeapi.com.br/json/daily/all/?start_date={anoInicial}{mesInicial}{diaInicial}&end_date={anoFinal}{mesFinal}{diaFinal}"
+    
+    else:
+        pass
 
     # link = f'https://economia.awesomeapi.com.br/json/daily/USD-BRL?start_date={anoInicial}{mesInicial}{diaInicial}&end_date={anoFinal}{mesFinal}{diaFinal}'
 
@@ -50,15 +90,6 @@ def atualizar_arq():
     # valor = cotacao[0]['bid']
 
     print(cotacao, "\n\n", reqCota, "\n\n", link)
-
-
-def create_moeda():
-    if (respcheck.get() == 0):
-        moeda2['state'] = 'normal'
-        selcMoeda2['state'] = 'normal'
-    else:
-        moeda2['state'] = 'disabled'
-        selcMoeda2['state'] = 'disabled'
 
 
 janela = tk.Tk()
@@ -97,7 +128,7 @@ botaoBusca = tk.Button(text="Buscar", command=pegar_cotacao, width=10, font="Rob
 
 
 result1 = tk.Label(text="", font="Roboto, 11")
-result1.grid(row=5, pady=(10, 20), column=0, columnspan=6, sticky="NSEW")
+result1.grid(row=5, pady=(10, 10), column=0, columnspan=6, sticky="NSEW")
 
 # Cotação de Várias Moedas
 
@@ -105,38 +136,45 @@ subTitle2 = tk.Label(text="Cotação de moedas + data", anchor="w", padx=15,
                      font=("Roboto, 14")).grid(row=6, column=0, sticky="NSEW")
 
 
-respcheck = tk.IntVar()
+selcArq = tk.Button(text="Selecione o arquivo", font="Roboto, 11", command=selecionar)
+selcArq.grid(row=7, column=0, pady=10, padx=(5, 0), sticky="NS")
 
-todasounnao = tk.Checkbutton(text="Quero salvar os dados de todas as moedas.", variable=respcheck, command= create_moeda).grid(
-    row=7, column=0, columnspan=7)
+caminho = tk.StringVar()
+caminho = ""
+
+opcional = tk.Label(text= "*Opcional").grid(row=7, column=0, padx= (230, 0), pady= (0, 10))
+
+
+respcheck = tk.IntVar()
+todasounnao = tk.Checkbutton(text="Quero salvar os dados de todas as moedas.", variable=respcheck, command= all_or_not).grid(
+    row=9, column=1, columnspan=7)
 
 
 moeda2 = tk.Label(text="Escolha a moeda: ", font=("Roboto, 11"))
-moeda2.grid(row=8, column=0, sticky="NSEW", pady=10)
+moeda2.grid(row=9, column=0, sticky="NSEW", pady=10)
 
 selcMoeda2 = ttk.Combobox(values=lista, width=15)
-selcMoeda2.grid(row=8, column=1, pady=10, sticky="NSEW")
+selcMoeda2.grid(row=9, column=0, pady=10, columnspan= 3)
 
+addMoeda = tk.Button(text= "+", font="Roboto, 9", command= adicionar_moeda)
+addMoeda.grid(row= 9, column= 1, padx= (40, 0))
 
 dataInicial = tk.Label(text="Escolha a data: ", font=("Roboto, 11")).grid(
-    row=9, column=0, sticky="NSEW", pady=(0, 5))
+    row=10, column=0, sticky="NSEW", pady=(0, 5))
 
 selcDataInicial = DateEntry(year=2022, locale='pt_br', width=15)
-selcDataInicial.grid(row=9, column=1, sticky="EW")
+selcDataInicial.grid(row=10, column=0, columnspan= 3)
 
 selcDataFinal = DateEntry(year=2022, locale='pt_br', width=15)
-selcDataFinal.grid(row=10, column=1, sticky="NSEW")
-
+selcDataFinal.grid(row=11, column=0, columnspan= 3)
 
 
 botaoSalvar = tk.Button(text="Salvar", font="Roboto, 13", width=15, height=2, command=atualizar_arq).grid(
-    row=7, column=2, rowspan=3, padx=(50, 0), pady=(5, 0))
+    row=8, column=2, rowspan=4, padx=(50, 0), pady=(50, 0))
 
 
-msgSelcErr = tk.Label(text="",
-                      font="Roboto, 12", anchor="w", justify="left")
-
-msgSelcErr.grid(row=11, column=2, sticky="NSEW", pady=(10, 40))
+msgSelcErr = tk.Label(text="Nenhum arquivo selecionado.",font="Roboto, 12")
+msgSelcErr.grid(row=12, column=0, sticky="NSEW", columnspan= 5, pady=(25, 40))
 
 
 janela.mainloop()
@@ -144,4 +182,4 @@ janela.mainloop()
 
 # Pegar selec arquiv (colocar nome de atualizar arquivo) no github
 # Fazer com que o sistema crie um arquivo novo com os dados ou atualize um arquivo
-# Fazer com que o usuário possa escolher se quer todas as moedas ou só algumas
+# Salvar os nomes das moedas no array. pos = soma dos valores ASCII do nome da moeda
